4b98d55ba8a612c9eb2516f01205a1261f32c2ef
Richard Greene <rhgreene110@mac.com>
2019-09-08 09:48:17 -0500

    Firth update Test 2

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ class EchoBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    // await context.sendActivity('Hello and welcome to BOT5 test1!');
+                    // await context.sendActivity('Hello and welcome to BOT5 test2!');
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
